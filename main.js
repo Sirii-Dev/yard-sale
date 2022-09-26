94c4eb7f72e83636dba52cb811fc37b90907361c
Sarahi Esquivel Pardo <sarahiesquivel45@gmail.com>
2022-09-26 16:17:01 -0500

    fusion del icono de hamburgesa con su menu deplegable

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const menuAccount = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu ");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamburgerIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 
 menuAccount.addEventListener("click", () => {
   desktopMenu.classList.toggle("inactive");
 });
+
+menuHamburgerIcon.addEventListener("click", () => {
+  mobileMenu.classList.toggle("inactive");
+});
+
+// function toggleMobileMenu() {
+//   mobileMenu.classList.toggle("inactive");
+// }
