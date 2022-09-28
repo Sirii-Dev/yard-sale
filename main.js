92ef679a515e62c20fc9269f11d84c2832422d34
Sarahi Esquivel Pardo <sarahiesquivel45@gmail.com>
2022-09-28 18:34:39 -0500

    fusion menu mobile y aparicion/desaparicion de menu

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 const menuAccount = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+
+const menuCarIcon = document.querySelector(".navbar-shopping-cart");
+const asideProductDetail = document.querySelector(".product-detail");
+
 const menuHamburgerIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 
-menuAccount.addEventListener("click", () => {
+menuAccount.addEventListener('click', showMenuDesktop);
+menuCarIcon.addEventListener('click', showProductDetail);
+menuHamburgerIcon.addEventListener('click', showMobileMenu);
+
+function showMenuDesktop(){
+  asideProductDetail.classList.add("inactive");
   desktopMenu.classList.toggle("inactive");
-});
+};
 
-menuHamburgerIcon.addEventListener("click", () => {
+function showProductDetail(){
+  desktopMenu.classList.add("inactive");
+  asideProductDetail.classList.toggle("inactive");
+}
+
+function showMobileMenu(){
+  asideProductDetail.classList.add("inactive");
   mobileMenu.classList.toggle("inactive");
-});
+}
 
-// function toggleMobileMenu() {
-//   mobileMenu.classList.toggle("inactive");
-// }
+
