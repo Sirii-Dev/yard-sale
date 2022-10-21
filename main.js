e82a65c9a7545d0d015a23f4a218fc79d43d6f7b
Sarahi Esquivel Pardo <sarahiesquivel45@gmail.com>
2022-10-20 22:19:52 -0500

    cambios a los estilos y background negro al abrir los menus en desktop

--- main.js
+++ main.js
@@ -12,30 +12,55 @@ const cardsContainer = document.querySelector(".cards-container");
 const productDetailContainer = document.querySelector("#product-detail");
 const productDetailClose = document.querySelector(".product-detail-close");
 
+const dark = document.querySelector(".dark");
+
+
+
 menuAccount.addEventListener('click', showMenuDesktop);
 menuCarIcon.addEventListener('click', showProductDetail);
 menuHamburgerIcon.addEventListener('click', showMobileMenu);
 productDetailClose.addEventListener('click', closeProductDetail);
 
+//Menu de la cuenta
 function showMenuDesktop(){
   desktopMenu.classList.toggle("inactive");
   productDetailContainer.classList.add('inactive');
   shoppingCart.classList.add("inactive");
+  //dark.classList.toggle("inactive");
+
+  const isMenuOpen = !desktopMenu.classList.contains('inactive');
+
+  if(isMenuOpen)
+      dark.classList.remove('inactive');
+  else
+      dark.classList.add('inactive');
+ 
 };
 
+//muestra productos dentro del carrito
 function showProductDetail(){
   shoppingCart.classList.toggle("inactive");
   desktopMenu.classList.add("inactive");
   productDetailContainer.classList.add("inactive");
   mobileMenu.classList.add("inactive");
+  //dark.classList.toggle("inactive");
+
+  isShoppingCartOpen = !shoppingCart.classList.contains("inactive");
+  if(isShoppingCartOpen){
+    dark.classList.remove('inactive');
+  }else {
+    dark.classList.add('inactive');
+  }
 }
 
+// Menu hamburgesa
 function showMobileMenu(){
   mobileMenu.classList.toggle("inactive");
   shoppingCart.classList.add("inactive");
   productDetailContainer.classList.add("inactive");
 }
 
+//menu de categorias de productos
 function openProductDetail(){
   productDetailContainer.classList.toggle('inactive');
   desktopMenu.classList.add("inactive");
@@ -45,7 +70,21 @@ function closeProductDetail () {
   productDetailContainer.classList.add('inactive');
 }
 
+function darkBackgorund(){
+  const isDesktopMenuOpen = desktop-menu.classList.contains("inactive");
+  const isShoppingCartOpen = shoppingCart.classList.contains("inactive");
+
+  if (isDesktopMenuOpen || isShoppingCartOpen){
+    dark.classList.toggle("inactive");
+  }
+}
+
 const productList = [];
+productList.push({
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
 productList.push({
   name: 'Bike',
   price: 120,
